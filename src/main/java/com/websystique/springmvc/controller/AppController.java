a794bb9da92a3fde32a6a52793e8d94255d65ec6
Monika <39961242+mopetkova@users.noreply.github.com>
2018-06-29 15:53:24 +0200

    Update AppController.java

--- src/main/java/com/websystique/springmvc/controller/AppController.java
+++ src/main/java/com/websystique/springmvc/controller/AppController.java
@@ -8,6 +8,7 @@ import com.websystique.springmvc.service.EmployeeService;
 
 import com.websystique.springmvc.service.MaritalStatusService;
 import com.websystique.springmvc.service.SkillService;
+import com.websystique.springmvc.utils.Notification;
 import com.websystique.springmvc.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,7 +22,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.websystique.springmvc.utils.Utils.isValid;
@@ -34,12 +38,12 @@ public class AppController {
 
     @Autowired
     EmployeeService service;
-
     @Autowired
     SkillService skillService;
-
     @Autowired
     MaritalStatusService maritalStatusService;
+
+    private List<Notification> notifications = new ArrayList<Notification>();
 
     @RequestMapping(method = RequestMethod.GET)
     public String welcome(ModelMap model) {
@@ -57,8 +61,10 @@ public class AppController {
         if (checkEmployee != null) {
             return homePage(model, null);
         } else {
-            model.addAttribute("error", "User not exist");
-            return "error";
+            notifications.clear();
+            notifications.add(new Notification("alert-danger", "User not found. Insert valid username and password."));
+            model.addAttribute("notifications", notifications);
+            return "welcome";
         }
 
     }
@@ -67,60 +73,26 @@ public class AppController {
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String homePage(ModelMap model, @RequestParam(value = "filter", required = false) String filter) {
         String u = new Utils("AppController", "homePage").toString();
-
-        model.addAttribute("filter", "");
-
         //TODO if utente in sessione? getEmployee()
 
         List<Employee> employees = service.findAllEmployees();
-
+        model.addAttribute("filter", "");
 
         if (isValid(filter)) {
-            u = new Utils("AppController", "homePage - isVALID").toString();
             employees = search(filter);
         }
 
         model.addAttribute("employees", employees);
 
+       // model.addAttribute("blabla", employeeSkill(employees.get(0)));
+
         return "allEmployees";
     }
 
 
-    @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public String registrationPost(@ModelAttribute("employee") Employee employee, BindingResult result, ModelMap model) {
-        String u = new Utils("AppController", "registrationPOST").toString();
-
-        Employee e = createEmployee(employee, result, model);
-        service.saveEmployee(e);
-
-        if (result.hasErrors()) {
-            model.addAttribute("error", "Errore durante la registrazione! \n" + result.toString());
-            return "error";
-        } else {
-            return homePage(model, null);
-        }
-
-    }
-
-    @RequestMapping(value = "/registration", method = RequestMethod.GET)
-    public String registrationGet(@ModelAttribute("employee") Employee employee, ModelMap model) {
-        String u = new Utils("AppController", "registrationGET").toString();
-        List<Skill> skills = skillService.allSkills();
-        List<MaritalStatus> maritalStatusList = maritalStatusService.allMaritalStatus();
-
-        model.addAttribute("employee", new Employee());
-        model.addAttribute("skills", skills);
-        model.addAttribute("maritalStatusList", maritalStatusList);
-
-        return "registration";
-    }
-
     @RequestMapping(value = "/update", method = RequestMethod.GET)
-//    public String updateEmployeeGet(@ModelAttribute("Employee") Employee Employee, BindingResult result, ModelMap model, HttpSession session) {
-    public String updateEmployeeGet(@ModelAttribute("employee") Employee employee,
-                                    @RequestParam("id") int employeeId, ModelMap model,
-                                    @ModelAttribute("maritalStatus") MaritalStatus maritalStatus
-                                   ) {
+    public String updateEmployeeGet(@ModelAttribute("employee") @Valid Employee employee,
+                                    @RequestParam("id") int employeeId, ModelMap model) {
         String u = new Utils("AppController", "updateEmployeeGET").toString();
 
         List<Skill> skills = skillService.allSkills();
@@ -135,43 +107,78 @@ public class AppController {
     }
 
     @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public String updateEmployeePost(@ModelAttribute("employee") Employee employee, BindingResult result, ModelMap model) {
+    public String updateEmployeePost(@ModelAttribute("employee") @Valid Employee employee, BindingResult result, ModelMap model) {
         String u = new Utils("AppController", "updateEmployeePOST").toString();
-
+//
+//        Employee e = service.getEmployeeById(employeeId);
+//
+//        if(e == null){
+//            notifications.clear();
+//            notifications.add(new Notification("alert-danger", "User not found."));
+//            model.addAttribute("notifications", notifications);
+//            return "update";
+//        }
+//        else {
         service.updateEmployee(employee);
 
         if (result.hasErrors()) {
-            model.addAttribute("error", "Errore durante update! \n" + result.toString());
-            return "error";
+            notifications.clear();
+            notifications.add(new Notification("alert-danger", "Error during the update."));
+            model.addAttribute("notifications", notifications);
+            return "update";
         } else {
+            notifications.clear();
+            notifications.add(new Notification("alert-success", "User updated successfully!"));
+            model.addAttribute("notifications", notifications);
             return homePage(model, null);
         }
 
 
     }
 
-    @RequestMapping(value = "/test", method = RequestMethod.POST)
-    public String test(@ModelAttribute("employee") Employee employee, BindingResult result, ModelMap model,
-                       @ModelAttribute("maritalStatus") MaritalStatus maritalStatus) {
-        String u = new Utils("AppController", "updateEmployeePOST").toString();
 
-        service.updateEmployee(employee);
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public String registrationPost(@ModelAttribute("employee") Employee employee, BindingResult result, ModelMap model) {
+        String u = new Utils("AppController", "registrationPOST").toString();
 
-        model.addAttribute("employee", employee);
-        model.addAttribute("model", model);
-        model.addAttribute("result", result);
-       // model.addAttribute("maritalStatus", maritalStatus);
+        if (checkValidEmployee(employee)) {
 
-        if (result.hasErrors()) {
-            model.addAttribute("error", "Errore durante update! \n" + result.toString());
-            return "error";
+            service.saveEmployee(employee);
+
+            if (result.hasErrors()) {
+                notifications.clear();
+                notifications.add(new Notification("alert-danger", "Error during registration."));
+                model.addAttribute("notifications", notifications);
+                model.addAttribute("error", "Errore durante la registrazione! \n" + result.toString());
+                return "error";
+            } else {
+                notifications.clear();
+                notifications.add(new Notification("alert-success", "User registered successfully!"));
+                model.addAttribute("notifications", notifications);
+                return homePage(model, null);
+            }
+
         } else {
-            return"test";
+            model.addAttribute("notifications", notifications);
+            return registrationGet(employee, model);
         }
 
-
-
     }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String registrationGet(@ModelAttribute("employee") Employee employee, ModelMap model) {
+        String u = new Utils("AppController", "registrationGET").toString();
+
+        List<Skill> skills = skillService.allSkills();
+        List<MaritalStatus> maritalStatusList = maritalStatusService.allMaritalStatus();
+
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("skills", skills);
+        model.addAttribute("maritalStatusList", maritalStatusList);
+
+        return "registration";
+    }
+
 
     @RequestMapping(value = "/delete", method = RequestMethod.GET)
     public String deleteEmployee(@ModelAttribute("employee") Employee employee, @RequestParam("id") int employeeId, BindingResult result, ModelMap model) {
@@ -181,39 +188,47 @@ public class AppController {
         service.deleteEmployee(employeeById);
 
         if (result.hasErrors()) {
-            model.addAttribute("error", "Errore durante delete! \n" + result.toString());
+            notifications.clear();
+            notifications.add(new Notification("alert-danger", "Error during delete."));
+            model.addAttribute("notifications", notifications);
+            model.addAttribute("error", "Error durante delete! \n" + result.toString());
             return "error";
         } else {
+            notifications.clear();
+            notifications.add(new Notification("alert-success", "User deleted successfully!"));
+            model.addAttribute("notifications", notifications);
             return homePage(model, null);
         }
     }
 
-    public Employee createEmployee(@ModelAttribute("employee") Employee employee, BindingResult result, ModelMap model) {
 
-        //TODO controllo sui campi, not empty
+    public boolean checkValidEmployee(@ModelAttribute("employee") Employee employee) {
+        String u = new Utils("AppController", "createEmployee").toString();
 
-        System.out.println(" - - - employee from post");
-        System.out.println(employee.toString());
+        boolean valid = false;
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        notifications.clear();
 
-        Employee e = new Employee();
 
-        e.setName(employee.getName());
-        e.setSurname(employee.getSurname());
-        e.setCountry(employee.getCountry());
-        if (employee.getBirthDate() != null) {
-            e.setBirthDate(employee.getBirthDate());
+        if (!isValid(employee.getName())) {
+            notifications.add(new Notification("alert-danger", "Name required."));
         }
-        if (employee.getMaritalStatus() != null) {
-            e.setMaritalStatusId(employee.getMaritalStatus());
+        if (!isValid(employee.getSurname())) {
+            notifications.add(new Notification("alert-danger", "Surname required."));
+        }
+//        if (!isValid(df.format(employee.getBirthDate()))) {
+//            notifications.add(new Notification("alert-danger", "Birth date required."));
+//        }
+        if (!isValid(employee.getUsername())) {
+            notifications.add(new Notification("alert-danger", "Username required."));
+        }
+        if (!isValid(employee.getPassword())) {
+            notifications.add(new Notification("alert-danger", "Password required."));
         } else {
-            System.out.println("---marital status null");
+            valid = true;
         }
 
-
-        e.setUsername(employee.getUsername());
-        e.setPassword(employee.getPassword());
-
-        return e;
+        return valid;
     }
 
     public List<Employee> search(String search) {
@@ -223,65 +238,29 @@ public class AppController {
         return filterList;
     }
 
+    public String employeeSkill(Employee employee) {
+        String u = new Utils("AppController", "employeeSkill").toString();
+        List<Skill> employeeSkill = employee.getSkills();
+        System.out.println(employeeSkill);
+        StringBuilder skill = new StringBuilder();
+        for (int i = 0; i < employeeSkill.size(); i++) {
+
+            skill.append(", ").append(employeeSkill.get(i).getName());
+        }
+        System.out.println(skill);
+        return skill.toString();
+    }
 
 
+    @ModelAttribute("maritalStatus")
+    public List<MaritalStatus> initializeMaritalStatus() {
 
-    /*
-     * This method will be called on form submission, handling POST request
-     * It also validates the Employee input
-     */
-//    @RequestMapping(method = RequestMethod.POST)
-//    public String saveRegistration(@Valid EmployeeForm EmployeeForm, BindingResult result, ModelMap model){
-//        String u = new Utils("AppController", "saveRegistration").toString();
-//        System.out.println("Result "+result);
-//        if(result.hasErrors()) {
-//            u = new Utils("AppController", "errorRegistration").toString();
-//            System.out.println("ErrorResult "+result);
-//            return "enroll";
-//        }
-//
-//        model.addAttribute("success", "Dear "+ EmployeeForm.getName()+" , your Registration completed successfully");
-//        return "success";
-//    }
+        return maritalStatusService.allMaritalStatus();
+    }
 
+    @ModelAttribute("skill")
+    public List<Skill> initializeSkill() {
+        return skillService.allSkills();
+    }
 
-//    @RequestMapping(value = "/login",method = RequestMethod.GET)
-//    public String login(ModelMap model) {
-//        String u = new Utils("AppController", "login").toString();
-//        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
-//        return "welcome";
-//    }
-//    @RequestMapping(method = RequestMethod.GET)
-//    public String index(ModelMap model) {
-//        String u = new Utils("AppController", "index").toString();
-//        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
-//        return "welcome";
-//    }
-
-//    @RequestMapping(value="/Employee", method=RequestMethod.GET)
-//    public String savePerson(@ModelAttribute EmployeeForm EmployeeForm, Model model) {
-//        String u = new Utils("AppController","savePerson").toString();
-//        model.addAttribute("EmployeeForm", EmployeeForm);
-//        return "Employee";
-//    }
-//
-//    @RequestMapping(value="/", method=RequestMethod.POST)
-//    public String savePersonPost(@Valid @ModelAttribute EmployeeForm personForm, BindingResult result, Model model) {
-//        String u = new Utils("AppController","savePersonPost").toString();
-//        if(result.hasErrors()) {
-//            model.addAttribute("personForm", personForm);
-//            return "Employee";
-//        }
-//        System.out.println(personForm.getName() + " " + personForm.getSurname());
-//        return "redirect:/";
-//    }
-
-//    @RequestMapping(value="/", method=RequestMethod.GET)
-//    public String updatePerson(@ModelAttribute EmployeeForm EmployeeForm, Model model) {
-//        String u = new Utils("AppController","updatePerson").toString();
-//        EmployeeForm.setName("Nome di default");
-//        EmployeeForm.setSurname("Cognome di default");
-//        model.addAttribute("EmployeeForm", EmployeeForm);
-//        return "Employee";
-//    }
 }
