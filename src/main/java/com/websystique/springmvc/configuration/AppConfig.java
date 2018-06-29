cdd7178cef2e9e38ce11686f350093a99177e224
Monika <39961242+mopetkova@users.noreply.github.com>
2018-06-29 15:51:08 +0200

    Update AppConfig.java

--- src/main/java/com/websystique/springmvc/configuration/AppConfig.java
+++ src/main/java/com/websystique/springmvc/configuration/AppConfig.java
@@ -1,19 +1,20 @@
 package com.websystique.springmvc.configuration;
 
+import com.websystique.springmvc.converters.MaritalStatusConverter;
+import com.websystique.springmvc.converters.SkillConverter;
+import com.websystique.springmvc.model.Skill;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.http.MediaType;
 import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
-
-
 
 
 @Configuration
@@ -21,41 +22,37 @@ import org.springframework.web.servlet.view.JstlView;
 @ComponentScan(basePackages = "com.websystique.springmvc")
 public class AppConfig extends WebMvcConfigurerAdapter {
 
-    /*
-     * Configure ContentNegotiationManager
-     */
+    @Autowired
+    public AppConfig() {
+
+    }
+    @Autowired
+    SkillConverter skillConverter;
+
+    @Autowired
+    MaritalStatusConverter maritalStatusConverter;
+
+
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.ignoreAcceptHeader(true).defaultContentType(
                 MediaType.TEXT_HTML);
     }
-    /*
-     * Configure ContentNegotiatingViewResolver
-     */
+
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(skillConverter);
+        registry.addConverter(maritalStatusConverter);
+    }
+
     @Bean
     public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
         ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
         resolver.setContentNegotiationManager(manager);
-
-//        // Define all possible view resolvers
-//        List<ViewResolver> resolvers = new ArrayList<ViewResolver>();
-//
-//        resolvers.add(jaxb2MarshallingXmlViewResolver());
-//        resolvers.add(jsonViewResolver());
-//        resolvers.add(jspViewResolver());
-//        resolvers.add(pdfViewResolver());
-//        resolvers.add(excelViewResolver());
-//
-//        resolver.setViewResolvers(resolvers);
         return resolver;
     }
 
-//    @Bean
-//    public ViewResolver jaxb2MarshallingXmlViewResolver() {
-//        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-//        marshaller.setClassesToBeBound(Employee.class);
-//        return new Jaxb2MarshallingXmlViewResolver(marshaller);
-//    }
 
     @Bean
     public ViewResolver jspViewResolver() {
@@ -66,6 +63,11 @@ public class AppConfig extends WebMvcConfigurerAdapter {
         return viewResolver;
     }
 
+    @Override
+    public void configurePathMatch(PathMatchConfigurer matcher) {
+
+        matcher.setUseRegisteredSuffixPatternMatch(true);
+    }
 
 
 }
